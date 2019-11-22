d5313eed359b962d23d05e3869320f9b03670ba4
Quesada Leon, Alberto (EXT) (SGRE CT DSF) <alberto.quesada.ext@siemensgamesa.com>
2019-11-22 14:06:19 +0100

    commit new

--- run_app.py
+++ run_app.py
@@ -5,10 +5,4 @@ Created on Fri Oct 18 09:40:57 2019
 @author: alberto.quesada
 """
 
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World!"
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=int("5000"), debug=True)
+print('hello world!!')
